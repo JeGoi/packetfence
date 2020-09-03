5ba42b6c7990a8d753820e154d89e5b90e523ce0
James Rouzier <jrouzier@inverse.ca>
2020-09-03 13:24:58 -0400

    Update database schema

--- db/upgrade-X.X.X-X.Y.Z.sql
+++ db/upgrade-X.X.X-X.Y.Z.sql
@@ -189,6 +189,10 @@ ALTER TABLE activation ADD COLUMN `category_id` int AFTER `unregdate`;
 ALTER TABLE node_category
   ADD COLUMN IF NOT EXISTS `parent_id` int NULL default NULL;
 
+!\ echo "Inserting new roles";
+INSERT INTO `node_category` (name,notes) VALUES ("User", "User role");
+INSERT INTO `node_category` (name,notes) VALUES ("Machine","Machine role");
+
 \! echo "Incrementing PacketFence schema version...";
 INSERT IGNORE INTO pf_version (id, version) VALUES (@VERSION_INT, CONCAT_WS('.', @MAJOR_VERSION, @MINOR_VERSION, @SUBMINOR_VERSION));
 
